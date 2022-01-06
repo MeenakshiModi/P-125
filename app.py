e123f2577470ebdf4f1ac823f156a4c1ea092acd
MeenakshiModi <71830157+MeenakshiModi@users.noreply.github.com>
2022-01-06 14:45:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request
+from classifier import  get_prediction
+
+app = Flask(_name_)
+
+@app.route("/predict-alphabet", methods=["POST"])
+def predict_data():
+  image = request.files.get("alphabet")
+  prediction = get_prediction(image)
+  return jsonify({
+    "prediction": prediction
+  }), 200
+
+if _name_ == "_main_":
+  app.run(debug=True)
